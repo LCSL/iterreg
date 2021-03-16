728e31b5e6e6551ef98d8a37aab012f669ece0e1
mathurinm <mathurin.massias@gmail.com>
2021-03-16 10:58:53 +0100

    Add Lasso comparison

--- examples/plot_stepsize_influence.py
+++ examples/plot_stepsize_influence.py
@@ -10,14 +10,13 @@ but is beneficial for support recovery.
 import numpy as np
 from numpy.linalg import norm
 import matplotlib.pyplot as plt
-from sklearn.metrics import f1_score, mean_squared_error
-from sklearn.model_selection import train_test_split, KFold
-from celer import LassoCV
+from sklearn.metrics import f1_score
+from sklearn.model_selection import GridSearchCV
+from celer import Lasso
 from celer.datasets import make_correlated_data
 from celer.plot_utils import configure_plt
 
-from iterreg import BasisPursuitIterReg
-from iterreg.ell1 import primal_dual, dual_primal
+from iterreg.ell1 import dual_primal
 
 
 configure_plt(fontsize=16)
@@ -35,7 +34,7 @@ def plot_varying_sigma(A, b, x_true, steps, max_iter=100):
             A, b, step=step, ret_all=True, max_iter=max_iter, f_store=1)
         scores = [f1_score(x != 0, x_true != 0) for x in all_x]
         supp_size = np.sum(all_x != 0, axis=1)
-        axarr[0, i].set_title(f"$\sigma=1 /{step} ||A||$", fontsize=20)
+        axarr[0, i].set_title(r"$\sigma=1 /%d ||A||$" % step, fontsize=20)
 
         axarr[0, i].plot(scores)
         axarr[0, i].set_ylim(0, 1)
@@ -58,24 +57,9 @@ def plot_varying_sigma(A, b, x_true, steps, max_iter=100):
 
 ###############################################################################
 # Noiseless case where RIP holds (L1 sol = L0 sol)
-A_, b_, x_true = make_correlated_data(
-    n_samples=1000, n_features=2000, density=0.01, corr=0., snr=np.inf,
+A, b, x_true = make_correlated_data(
+    n_samples=500, n_features=1000, density=0.01, corr=0., snr=np.inf,
     random_state=0)
-
-A, A_test, b, b_test = train_test_split(A_, b_, test_size=0.25, random_state=0)
-
-clf = BasisPursuitIterReg(verbose=0).fit(A, b)
-
-plt.figure(figsize=(6, 3), constrained_layout=True)
-plt.semilogy(clf.mses)
-plt.axvline(np.argmin(clf.mses), c='k', linestyle='--', label='best iteration')
-plt.axhline(mean_squared_error(A_test @ x_true, b_test),
-            c='k', label='oracle MSE')
-plt.xlabel("Chambolle-Pock iteration")
-plt.ylabel('Left out MSE')
-plt.legend()
-plt.show(block=False)
-
 
 plot_varying_sigma(A, b, x_true, [2, 10, 100], max_iter=100)
 
@@ -114,3 +98,23 @@ A, b, x_true = make_correlated_data(
     n_samples=1000, n_features=2000, density=density, corr=corr, snr=snr,
     random_state=0)
 plot_varying_sigma(A, b, x_true, [2, 10, 100], max_iter=100)
+
+
+###############################################################################
+# On last example, compare with Lasso
+
+def f1(clf, X, y):
+    return f1_score(clf.coef_ != 0, x_true != 0)
+
+
+clf = Lasso(fit_intercept=False)
+alphas = norm(A.T @ b, ord=np.inf) / len(b) * np.geomspace(1, 1e-3)
+grid_search = GridSearchCV(
+    clf, {'alpha': alphas}, scoring=f1, refit=False, cv=3).fit(A, b)
+
+plt.figure(figsize=(5, 3), constrained_layout=True)
+plt.semilogx(alphas, grid_search.cv_results_["mean_test_score"])
+plt.xlabel(r'$\lambda$')
+plt.ylabel("F1 score for support")
+plt.title("Lasso regularization path")
+plt.show(block=False)
