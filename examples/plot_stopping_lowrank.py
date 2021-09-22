95f4b0f0c5c9d1fc3f79e10db15b05665ef0678c
mathurinm <mathurinm@users.noreply.github.com>
2021-09-22 16:03:13 +0200

    DOC: Low rank expe (#30)
    * tunable sigma in stopping_lowrank
    
    * Add stop crit for low rank solver, better low rank expe
    
    * fix

--- examples/plot_stopping_lowrank.py
+++ examples/plot_stopping_lowrank.py
@@ -16,21 +16,23 @@ from iterreg.low_rank.solvers import dual_primal_low_rank
 configure_plt()
 
 
-d = 20
+d = 100
 np.random.seed(0)
 mask = np.zeros([d, d], dtype=bool)
 idx = np.random.choice(d ** 2, d ** 2 // 5, replace=False)
 mask.flat[idx] = True
-
-Y_true = np.random.randn(d, 5) @ np.random.randn(5, d)
+# rank = d // 10
+rank = 5
+Y_true = np.random.randn(d, rank) @ np.random.randn(rank, d)
 Y_true /= (norm(Y_true, ord="fro") / 20)
 Y = Y_true.copy()
 
 Y[~mask] = 0
 
 W_star, Theta, _ = dual_primal_low_rank(
-    mask, Y, max_iter=20000, f_store=100, verbose=0)
+    mask, Y, max_iter=3_000, stop_crit=1e-8, f_store=100, verbose=1)
 
+print(f"Feasability of W_star : {norm((Y - W_star)[mask]):.2e}")
 
 n_deltas = 10
 deltas = np.linspace(1, 15, num=n_deltas)
@@ -39,23 +41,38 @@ noise = np.random.randn(d, d)
 distances = dict()
 f_store = 1
 
+deltas = deltas[3:-1]
+
 for delta in deltas:
     print(delta)
-    Y_delta = Y + delta * noise / norm(noise)
-    w, theta, dist = dual_primal_low_rank(
-        mask, Y_delta, max_iter=1000, verbose=False, f_store=f_store,
-        limit=W_star)
+    Y_delta = Y_true + delta * noise / norm(noise)
+    sigma = 1 / norm(Y_delta, ord=2)
+
+    x, theta, dist = dual_primal_low_rank(
+        mask, Y_delta, max_iter=200, sigma=sigma, verbose=False,
+        f_store=f_store, limit=W_star)
 
     distances[delta] = dist
 
-fig1, ax = plt.subplots(1, 1, constrained_layout=True, figsize=(8, 4))
-n_points = 500
-for delta in deltas[3:-1]:
+plt.close('all')
+fig1, ax = plt.subplots(1, 1, constrained_layout=True, figsize=(3.8, 2.2))
+n_points = 100
+for delta in deltas:
     x_plt = f_store * np.arange(len(distances[delta]))
     y_plt = distances[delta] / norm(W_star)
     ax.semilogy(x_plt[:n_points], y_plt[:n_points],
                 label=r"$\delta={:.1f}$".format(delta))
-plt.ylabel(r'$||w_k^\delta - w^\star|| / ||w^\star||$')
-plt.xlabel("Iteration $k$")
-plt.legend(loc='upper right', ncol=3, fontsize=16)
+
+paper = False
+if paper:
+    plt.ylabel(r'$||X_k - {X}^\star|| / ||{X}^\star||$')
+    plt.xlabel("iterative regularization iteration $k$")
+else:
+    plt.ylabel(r'$||W_k - {W}^\star|| / ||{W}^\star||$')
+    plt.legend(loc='upper right', ncol=3, fontsize=16)
+
 plt.show(block=False)
+
+
+if paper:
+    fig1.savefig("low_rank_d%d.pdf" % d)
--- iterreg/low_rank/solvers.py
+++ iterreg/low_rank/solvers.py
@@ -5,13 +5,18 @@ from iterreg.utils import shrink
 
 
 def dual_primal_low_rank(
-        mask, Y, max_iter=1000, f_store=10, limit=None, verbose=False):
+        mask, Y, max_iter=1000, f_store=10, sigma=None, limit=None,
+        stop_crit=1e-10, verbose=False):
     """Lowest nuclear norm matrix equal to Y on mask.
     mask and Y are np.arrays, shape (d, d).
     """
     d = mask.shape[0]
-    sigma = 0.99
-    tau = 1.
+
+    if sigma is None:
+        sigma = 0.99
+        tau = 1.
+    else:
+        tau = 0.99 / sigma
     Theta = np.zeros((d, d))
     Theta_old = Theta.copy()
     distances = np.zeros(max_iter // f_store)
@@ -27,7 +32,12 @@ def dual_primal_low_rank(
         if k % f_store == 0:
             if limit is not None:
                 distances[k // f_store] = norm(W - limit)
+            feasability = norm((W - Y)[mask])
             if verbose:
-                print("Iter %d" % k)
+                print(f"Iter {k}, feasability: {feasability:.1e}")
+            if feasability < stop_crit:
+                print(
+                    f"Feasability {feasability:.1e} < {stop_crit:.1e}, exit.")
+                break
 
     return W, Theta, distances
