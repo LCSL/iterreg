b5fa68c21ad57cc9fddf86a2afe391bf6f0fcd4f
mathurinm <mathurin.massias@gmail.com>
2021-09-22 15:40:24 +0200

    Add stop crit for low rank solver, better low rank expe

--- iterreg/low_rank/solvers.py
+++ iterreg/low_rank/solvers.py
@@ -6,7 +6,7 @@ from iterreg.utils import shrink
 
 def dual_primal_low_rank(
         mask, Y, max_iter=1000, f_store=10, sigma=None, limit=None,
-        verbose=False):
+        stop_crit=1e-10, verbose=False):
     """Lowest nuclear norm matrix equal to Y on mask.
     mask and Y are np.arrays, shape (d, d).
     """
@@ -32,7 +32,12 @@ def dual_primal_low_rank(
         if k % f_store == 0:
             if limit is not None:
                 distances[k // f_store] = norm(W - limit)
+            feasability = norm((W - Y)[mask])
             if verbose:
-                print("Iter %d" % k)
+                print(f"Iter {k}, feasability: {feasability:.1e}")
+            if feasability < stop_crit:
+                print(
+                    f"Feasability {feasability:.1e} < {stop_crit:.1e}, exit.")
+                break
 
     return W, Theta, distances
